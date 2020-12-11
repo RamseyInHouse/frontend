b6630d069d0cf0bcebdbbf44fde6addca437787d
Christian Hughes <christian.hughes@daveramsey.com>
2020-12-11 09:17:42 -0600

    New package: @ramseyinhouse/stylelint-config (#10)
    * Initial draft for stylelint-config package.
    
    * Update READMEs
    
    * Remove redundant prettier use.
    
    * Update packages/stylelint-config/package.json
    
    Co-authored-by: Alex MacArthur <alex@macarthur.me>
    
    * Update packages/stylelint-config/package.json
    
    Co-authored-by: Alex MacArthur <alex@macarthur.me>
    
    Co-authored-by: Alex MacArthur <alex@macarthur.me>

---
+++ packages/stylelint-config/lib/selectorClassPattern.js
@@ -0,0 +1,18 @@
+const selectorClassPatternRules = {
+  components:
+    '^([a-z]+-)?([A-Z][a-zA-Z0-9]+)(-[a-zA-Z0-9]+)?(--[a-zA-Z0-9]+)?$',
+  utilities:
+    '^u-((|palmOnly|lapOnly|lapToLapWide|lapToDesk|lapAndUp|lapWideOnly|lapWideToDesk|lapWideAndUp|deskOnly|deskAndUp|wallOnly)-)?[a-z][a-zA-Z0-9]+$',
+  states: '^(has|is|in)-[a-z][A-Za-z0-9]+$',
+};
+
+function getFormattedSelectorClassPatternRules() {
+  return Object.values(selectorClassPatternRules)
+    .map((rule) => `(${rule})`)
+    .join('|');
+}
+
+module.exports = {
+  selectorClassPatternRules,
+  getFormattedSelectorClassPatternRules,
+};
